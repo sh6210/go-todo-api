a95ae3f16e75196c0fabfa2f32e19da68493723b
sh6210 <shcseilacs@gmail.com>
2026-08-06 23:12:39 +0600

    WIP: created model named todo.go, docs will be written later

---
+++ internal/models/todo.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Todo struct {
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Completed   bool      `json:"completed"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type CreateTodoRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type UpdateTodoRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
