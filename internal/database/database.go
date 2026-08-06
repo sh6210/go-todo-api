bc1ea3c9f6b265c490b25fe87c4021963b3913a9
sh6210 <shcseilacs@gmail.com>
2026-08-06 23:12:22 +0600

    WIP: created database.go, docs will be written later

---
+++ internal/database/database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewConnection(databaseURL string) (*pgxpool.Pool, error) {
+	// context.WithTimeout, will try for 5 seconds, if it doesn't work, it'll terminate
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	return pool, nil
+}
