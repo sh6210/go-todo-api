58304a4f481fba4a4e2542837706b859b9285595
sh6210 <shcseilacs@gmail.com>
2026-08-10 11:16:46 +0600

    implemented Gracefully Shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/sh6210/go-todo-api/internal/config"
 	"github.com/sh6210/go-todo-api/internal/database"
@@ -21,19 +25,39 @@ func main() {
 		appLogger.Error("failed to connect to database", "error", err)
 		os.Exit(1)
 	}
-
 	defer db.Close()
 
 	todoRepo := repository.NewTodoRepository(db)
-
 	todoHandler := handlers.NewTodoHandler(todoRepo)
-
 	//router := handlers.NewRouter(todoHandler)
 	router := handlers.NewRouterWithLogger(todoHandler, appLogger)
 
-	log.Printf("Listening on port %s", cfg.Port)
-
-	if err := http.ListenAndServe(":"+cfg.Port, router); err != nil {
-		log.Fatalf("failed to start server: %v", err)
+	srv := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: router,
 	}
+
+	go func() {
+		appLogger.Info("server starting", "port", cfg.Port)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			appLogger.Error("server shutdown", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	appLogger.Info("shutting signal received, starting graceful shutdown")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		appLogger.Error("failed to shutdown server", "error", err)
+		os.Exit(1)
+	}
+
+	appLogger.Info("shutting down graceful shutdown")
 }
